2e63f93f2e88628b0b06518873866fa3803fc1d3
Laura Macias <melodyinsideme@gmail.com>
2022-06-23 20:51:28 -0400

    added edit function and cancel/update buttons

--- posts/views.py
+++ posts/views.py
@@ -34,3 +34,24 @@ def delete(request, post_id):
     post = Post.objects.get(id = post_id)
     post.delete()
     return HttpResponseRedirect('/')
+
+def edit(request, post_id):
+    post = Post.objects.get(id=post_id)
+    # Find post
+    # if request.method == "GET":
+    # post = Post.objects.get(id=post_id)
+    if request.method == 'POST':
+        # editpost = Post.objects.get(id=post_id)
+        form = PostForm(request.POST, request.FILES, instance=post)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/')
+        else:
+            return HttpResponse("not valid")
+
+    form = PostForm
+    # form = PostForm
+
+    # show
+    return render(request, 'edit.html', {'post': post, 'form': form})
+
